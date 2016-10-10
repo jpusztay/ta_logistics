548660b264ce89391bd6169f7a4f071a164e5d72
Jordan Hoeber <jrhoeber@buffalo.edu>
2016-10-10 01:53:44 -0400

    Added some views

--- ta_logistics_application/urls.py
+++ ta_logistics_application/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from . import views
 
-app_name = 'ta_logic'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-   # url(r'^$', views.index, name='application'),
-    url(r'^submit/$', views.submit, name='submit')
+    url(r'^submit/$', views.submit, name='submit'),
+    url(r'^student/profile/$', views.student_profile, name='student_profile'),
+    url(r'^student/profile/edit$', views.edit_student_profile, name='edit_student_profile'),
 ]
--- ta_logistics_application/views.py
+++ ta_logistics_application/views.py
@@ -16,8 +16,23 @@ def submit(request):
         if form.is_valid():
             # file is saved
             form.save()
-            template = loader.get_template('ta_logistics_application/submission_received.html')
+            template = loader.get_template('ta_logistics_application/student/submission_received.html')
             return HttpResponse(template.render())
     else:
         form = ResumeForm()
-    return render(request, 'ta_logistics_application/application.html', {'form': form})
+    return render(request, 'ta_logistics_application/student/application.html', {'form': form})
+
+def edit_student_profile(request):
+    if request.method == 'POST':
+        form = ResumeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            template = loader.get_template('ta_logistics_application/student/edit_profile.html')
+            return HttpResponse(template.render())
+    else:
+        form = ResumeForm()
+    return render(request, 'ta_logistics_application/student/edit_profile.html', {'form': form})
+
+def student_profile(request):
+    template = loader.get_template('ta_logistics_application/student/profile.html')
+    return HttpResponse(template.render())
