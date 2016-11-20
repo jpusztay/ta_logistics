79f2879de86cc71e2cb3b889f147b2c92689c0b0
Jordan Hoeber <jrhoeber@buffalo.edu>
2016-11-20 15:59:52 -0500

    More status changes

---
+++ ta_logistics_application/static/js/student_index.js
@@ -0,0 +1,15 @@
+$( document ).ready( function () {
+
+   $('#applications_table').DataTable({
+        "paging": false,
+        "info": false,
+        "searching": false
+    });
+
+   $('#offers_table').DataTable({
+        "paging": false,
+        "info": false,
+        "searching": false
+    });
+
+});
